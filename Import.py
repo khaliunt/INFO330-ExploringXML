5fbcdf93041c3b3448726549102000971c882dc6
khaliunt <khaliunt@uw.edu>
2023-05-09 21:16:01 -0700

    hw

--- Import.py
+++ Import.py
@@ -21,15 +21,30 @@ import xml.etree.ElementTree as ET
 #     </abilities>
 # </pokemon>
 
-
+# New Pokemon into the database
 
 # Read pokemon XML file name from command-line
 # (Currently this code does nothing; your job is to fix that!)
 if len(sys.argv) < 2:
     print("You must pass at least one XML file name containing Pokemon to insert")
 
+conn = sqlite3.connect("pokemon.sqlite")
+cursor = conn.cursor()
+
+
 for i, arg in enumerate(sys.argv):
     # Skip if this is the Python filename (argv[0])
     if i == 0:
         continue
+
+tree = ET.parse(arg)
+root = tree.getroot()
+for pokemon in root.findall("pokemon"):
+        insert_pokemon(cursor, pokemon)
+    
+conn.commit()
+conn.close()
+
+
+
 
